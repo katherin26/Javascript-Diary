2e62ab64faaa077569be1d643b7327d081cc3d7e
katherin <ft.katheochoa@gmail.com>
2021-08-04 21:55:58 -0400

    added debugger and thermometer exercise

--- Fundamentals-02/script.js
+++ Fundamentals-02/script.js
@@ -610,3 +610,53 @@ const measureKelvin = function () {
 };
 
 console.log(measureKelvin());
+
+//------------------------------USING A DEBUGGER-----------------------------------------------------------//
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps); // array concatenado
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger; // Es un breakpoint para determinar donde ahi que aplicar el debugger.
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+//console.log(amplitudeBug); // ===> Sources (Debugger paused)
+
+//----------------------THERMOMETER EXERCISE------------------------------------------------------------//
+// Given an array of forecasted maximun temperatures , the thermometer displays a string with these temperatures.
+
+/*example : [17,21,23] will print "...17C in 1 days .. 21C in 2 days ... 23C in 3 days..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17,21,23];
+TEST DATA 2: [12,5,-5,0,4];*/
+
+const printForecast = function (arr) {
+  let str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}C in ${i + 1} days...`;
+  }
+  console.log('...' + str);
+};
+
+let data = [17, 21, 23];
+let data2 = [12, 5 - 5, 0, 4];
+console.log(printForecast(data));
+console.log(printForecast(data2));
