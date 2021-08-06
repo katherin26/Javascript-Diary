5f7a9eb7cf030fb4f691fad767f8bc84032d0b25
katherin <ft.katheochoa@gmail.com>
2021-08-06 16:53:59 -0400

    This in practice

--- Functions-This-hoisting/script.js
+++ Functions-This-hoisting/script.js
@@ -112,4 +112,140 @@ const person = {
 };
 console.log(person.calcAge()); //32
 
-//--------------------------------------------------//
+//------------------------THIS IN PRACTICE--------------------------//
+
+console.log(this); //Window {window: Window,...}
+
+const calcAge2 = function (birthYear) {
+  console.log(2021 - birthYear); //29
+  console.log(this); //undefined
+};
+
+calcAge2(1992);
+//-------//
+
+const calcAgeArrow2 = birthYear => {
+  console.log(2037 - birthYear); //45
+  console.log(this); //siempre usa el lexico this keyword. //Window {window: Window,...}
+};
+
+calcAgeArrow2(1992);
+//-------//
+
+const person2 = {
+  year: 1992,
+  calcAge2: function () {
+    console.log(this); //{year:1992,calcAge: f}
+    console.log(2037 - this.year); //45
+  },
+};
+
+person2.calcAge2();
+
+//-------//
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge2 = person2.calcAge2; // aqui se esta copiando el metodo que esta en person2.
+console.log(matilda.calcAge2()); //20
+
+//----------------REGULAR FUNCTIONS VS ARROW FUNCTIONS---------------//
+console.log(`//----------------------//`);
+const human = {
+  firstName: 'Humanito',
+  year: 1991,
+  calcAge3: function () {
+    console.log(this); //este this = human object.
+    console.log(2037 - this.year); //46
+
+    //---OLD SOLUTION === SELF OR THAT SOLUTION FOR REGULAR FUNCTIONS = UNDEFINED ----
+    /*
+    const self = this; //self or that.
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996); // self or that para que regular function = no sea undefined.
+      // console.log(this.year >= 1981 && this.year <= 1996); // this is undefined = porque es una regular function.
+    };
+    isMillenial();
+  },*/
+
+    //---NEW ES6 SOLUTION ARROW FUNCTION === FUNCIONA PORQUE NO TIENE SU PROPIO THIS KEYWORD
+
+    const isMillenial = () => {
+      console.log(this); //{ name: 'Humanito', year: 1991, calcAge3: [Function: calcAge3] }
+      console.log(this.year >= 1981 && this.year <= 1996); //This is undefined = porque es un regular function.
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(`Hey ${firstName}`);
+  },
+};
+
+human.greet(); //Hey undefined , porque arrow function no tiene su this keyword.
+//solamente usara el parent scope del greet que seria human.calcAge();
+human.calcAge3();
+
+//----------ARGUMENTS KEYWORD - para tenerlo encuenta pero ya ahi otra solucion mas novedosa------------------//
+/*
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);*/
+
+//-------------PRIMITIVES VS OBJECTS IN PRACTICE----------------//
+//PRIMITIVE TYPES
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName); //Davis Williams
+
+//REFERENCE TYPES
+
+const robot = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedRobot = robot;
+marriedRobot.lastName = 'Davis';
+console.log('Before Marriage', robot);
+console.log('After marriage', marriedRobot);
+
+//COPIA POR REFERENCIA = javaScript para no consumir mas memoria hace una copia por referencia cuando el valor es de tipo compuesto = objects, arrays ..etc.
+//por eso modifica el value del primer object.
+
+// RESULT = Before Marriage { firstName: 'Jessica', lastName: 'Davis', age: 27 }
+//After marriage { firstName: 'Jessica', lastName: 'Davis', age: 27 }
+
+//-------//
+
+const robot2 = {
+  firstName: 'Jessica2',
+  lastName: 'Williams2',
+  age: 26,
+  family: ['Alice', 'Bob'],
+};
+
+const robot2Copy = Object.assign({}, robot2); //sale un object nuevo y se guarda en robot2Copy.
+robot2Copy.lastName = 'Davis';
+console.log(`Before Marriage`, robot2);
+console.log(`After marriage:`, robot2Copy);
+
+robot2Copy.family.push('Mary');
+robot2Copy.family.push('John');
+
+const robot2Copy = { ...robot2 }; //ASSIGN Y ... agregan y modifican el array de los dos objects.
+console.log();
