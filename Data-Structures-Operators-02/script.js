15db6931fa1312ec5f7607aa4dca66e2930cb8c5
katherin <ft.katheochoa@gmail.com>
2021-08-12 21:44:08 -0400

    challenge string

--- Data-Structures-Operators-02/script.js
+++ Data-Structures-Operators-02/script.js
@@ -218,3 +218,107 @@ for (const [min, value] of gameEvents) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
   console.log(`${half} [HALF] ${min}: ${event}`);
 }
+
+//TITTLE:--------------------------CHALLENGE WITH STRING------------------------------//
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.body.append(document.createElement('p'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+
+  //NOTE: SPLIT THE TEXT
+  let list = text.split('\n');
+  // console.log(list);//split lo convierte en array DE 5 ELEMENTOS.
+
+  //NOTE: FOR LOOP AND INVOKING A FUNCTION
+  let newArr = []; //THis solution when you use  a array variable.
+  let newList = ''; //This solution when you use a string variable.
+  for (let [i, text] of list.entries()) {
+    let newText = toCamelCase(text);
+    newArr.push(newText);
+    newList += newText + `\n`;
+  }
+
+  //NOTE: FUNCTION TO CAMELCASE WITH DESTRUCTURING
+  function toCamelCase(str) {
+    const [a, b] = str.trim().split('_');
+    return a.toLowerCase() + b[0].toUpperCase() + b.slice(1).toLowerCase();
+  }
+
+  document.querySelector('p').textContent = newArr.join('\n'); //Show the new list.
+  console.log(newList); //Show the new list in the console.
+});
+
+//NOTE: ---------------------------SOLUTION WITH REPLACE---------------------------------//
+/*
+document.querySelector('button'),
+  addEventListener('click', function () {
+    const text = document.querySelector('texttarea').value;
+    const rows = text.split('\n');
+    console.log(rows);
+
+    for (const [i, row] of rows.entries()) {
+      //added desctrunturing and entries() + repeat(i+1)
+      const [first, second] = row.toLowerCase().trim().split('_'); //aqui ya se hace el lowercase , trim y split de todo el row para que despues se haga el uppercase.
+      const output = `${first}${second.replace(
+        second[0],
+        second[0].toUpperCase()
+      )}`; //replace (variable name and index , el segundo parametro es por lo que se quiere reemplazar.)
+      console.log(`${output.padEnd(20)}${'*'.repeat(i + 1)}`);
+    }
+  });
+*/
+
+//TITTLE: ---------------------FLIGHT EXERCISE WITH STRING-----------------------------//
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+/* 🔴 Delayed Departure from FAO to TXL (11h25)
+             Arrival from BRU to FAO (11h45)
+  🔴 Delayed Arrival from HEL to FAO (12h05)
+           Departure from FAO to LIS (12h30)*/
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ''
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(36);
+  console.log(output);
+}
