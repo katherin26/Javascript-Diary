ac88f530ae59354c851451fe512b352740353147
katherin <ft.katheochoa@gmail.com>
2021-08-18 20:54:55 -0400

    Chaining Methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -435,3 +435,33 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1); //44
 console.log(avg2); //47.333
+
+//TITTLE: ----------------------------CHAINING METHODS----------------------------------------//
+//NOTE: 224 eurToUsd = 1.1 AND 57 movements
+/*1) */
+
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD); //5522.00
+
+//TITTLE: Coding Challenge #3
+
+/*
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an
+ arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+const calcAverageHumanAge2 = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg3 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+console.log(avg3); //4
+const avg4 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg4); //47.33
