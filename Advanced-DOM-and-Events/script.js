ec40c0f79dffcf20ecc0be6b992c0439a6695ce5
katherin <ft.katheochoa@gmail.com>
2021-08-26 19:35:12 -0400

    index.html-scss-script.js-img

---
+++ Advanced-DOM-and-Events/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//TITTLE: Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault(); // The page did not jump to the top
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//NOTE: Botton open modal
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+/*for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);*/
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
