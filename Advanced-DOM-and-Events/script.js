63848bd9f233fe0138fe5a1fcff0bffde5ee6f79
katherin <ft.katheochoa@gmail.com>
2021-08-27 16:28:00 -0400

    Smooth scroll

--- Advanced-DOM-and-Events/script.js
+++ Advanced-DOM-and-Events/script.js
@@ -34,11 +34,35 @@ document.addEventListener('keydown', function (e) {
 });
 
 //NOTE: IMPLEMENTING SMOOTH SCROLLING
+//We need these coordinates here to tell JavaScript where it should scroll to .
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  console.log(
+    s1coords
+  ); /*DOMRect {x: 0, y: 276.8000183105469, width: 1524, 
+    height: 1580.425048828125, top: 276.8000183105469, …}*/
+
+  console.log(
+    e.target.getBoundingClientRect()
+  ); /*DOMRect {x: 187, y: 194.5, width: 110, 
+  height: 27.80000114440918, top: 194.5, …}*/
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  //Current scroll (X/Y) 0 174.39999389648438
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  ); // height/width viewport 181 1524
+
+  //IMPORTANT: see Notes1.md for old browsers with window.scrollTo()
+
+  //NOTE: ScrollIntoView() works for modern browsers
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
