f341cdb2c2490df56af814f072224cec6c9a29a5
katherin <ft.katheochoa@gmail.com>
2021-08-31 18:32:22 -0400

    Basic Exercises

--- Fundamentals-02/assignments.js
+++ Fundamentals-02/assignments.js
@@ -255,3 +255,43 @@ for (let i = 0; i < populationsArr.length; i++) {
 }
 
 console.log(percentages2);
+
+/* LOOPING BACKWARDS AND LOOPS IN LOOPS
+
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway �
+*/
+
+let listOfNeighbours = [
+  ['Canada', 'Mexico'],
+  ['Spain'],
+  ['Norway', 'Sweden', 'Russia'],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(` Neighbours: ${listOfNeighbours[i][j]}`);
+  }
+}
+
+/* THE WHILE LOOP =
+
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop?
+*/
+
+const percentages3 = [];
+let i = 0;
+while (i < populationsArr.length) {
+  const perc = percentageOfWorld1(populationsArr[i]);
+  percentages3.push(perc);
+  i++;
+}
+console.log(percentages3);
