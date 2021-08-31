c1601c374b69e8bfa3595ea83629e2f2e5e24152
katherin <ft.katheochoa@gmail.com>
2021-08-31 13:16:23 -0400

    Basic Exercises

--- Fundamentals/assignments.js
+++ Fundamentals/assignments.js
@@ -152,3 +152,53 @@ change some variables in order to make the condition true (unless you live in
   
 Canada :D)
 */
+
+if (language === 'english' && population < 50 && !isIsland) {
+  console.log(`You should live in Greece`);
+} else {
+  console.log(`Greece does not meet all the criteria =(`);
+}
+
+/* THE SWITCH STATEMENT =
+1. Use a switch statement to log the following string for the given 'language':
+    chinese or mandarin: 'MOST number of native speakers!'
+    spanish: '2nd place in number of native speakers'
+    english: '3rd place'
+    hindi: 'Number 4'
+    arabic: '5th most spoken language'
+    for all other simply log 'Great language too :D'
+*/
+
+switch (language) {
+  case 'chinese':
+  case 'mandarin':
+    console.log(`MOST number of native speakers !`);
+    break;
+  case 'spanish':
+    console.log(`2nd place in number of native speakers`);
+    break;
+  case 'english':
+    console.log(`3rd place`);
+    break;
+  case 'hindi':
+    console.log(`Number 4`);
+    break;
+  case 'arabic':
+    console.log(`5th most spoken language`);
+    break;
+  default:
+    console.log(`For all other simply log Great language too`);
+}
+
+/* THE CONDITIONAL (TERNARY) OPERATOR =
+1. If you country's population is greater than 33 million , use the ternary operator to log a string like
+this to the console: `Greece's population is above average`. Otherwise , simply log `Greece's population is 
+below average`. Notice how only one word changes between these two sentences!.
+
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different 
+results , and set the population back to original.
+*/
+
+console.log(
+  `${country}'s population is ${population > 33 ? 'above' : 'below'} average`
+);
