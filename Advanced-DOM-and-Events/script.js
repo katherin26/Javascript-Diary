34d5349ef1c2834aefd6e508b79812c8249c31ae
katherin <ft.katheochoa@gmail.com>
2021-09-01 15:02:44 -0400

    Building a tabbed component

--- Advanced-DOM-and-Events/script.js
+++ Advanced-DOM-and-Events/script.js
@@ -108,3 +108,33 @@ const alertH1 = function (e) {
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+//TITTLE: BUILDING A TABBED COMPONENT
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//tabs.forEach(t => t.addEventListener('click', () => console.log('Tabbbb')));
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab'); //more closest parentElement
+  console.log(clicked);
+
+  //Guard clause
+  if (!clicked) return;
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+}); //<button class = "btn operations__tab....</button>"
