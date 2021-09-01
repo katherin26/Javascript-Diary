f709aa3d4825981fab1f0e0ad14d92b8fb6446c9
katherin <ft.katheochoa@gmail.com>
2021-09-01 15:39:00 -0400

    Menu fase animation

--- Advanced-DOM-and-Events/script.js
+++ Advanced-DOM-and-Events/script.js
@@ -4,10 +4,18 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+/*--*/
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+/*--*/
 const section1 = document.querySelector('#section--1');
+/*--*/
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+/*--*/
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault(); // The page did not jump to the top
@@ -111,12 +119,6 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 //TITTLE: BUILDING A TABBED COMPONENT
 
-const tabs = document.querySelectorAll('.operations__tab');
-
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
-
 //tabs.forEach(t => t.addEventListener('click', () => console.log('Tabbbb')));
 
 tabsContainer.addEventListener('click', function (e) {
@@ -138,3 +140,26 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 }); //<button class = "btn operations__tab....</button>"
+
+//TITTLE: MENU FASE ANIMATION
+//The opposite of mouseenter is mouseleave
+//The oposite of mouseover is mouseout
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this; //the this keyword is now our opacity.
+  }
+};
+
+// Passing "argument" into handler.
+/*The bind method creates a copy of the function that it's called on and it will set the disc keyword
+in this function call to whatever value that we pass into bind.*/
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
