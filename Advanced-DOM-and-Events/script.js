162dc0603bf9015f5bed635ae211e1fb15cc870a
katherin <ft.katheochoa@gmail.com>
2021-09-01 21:31:16 -0400

    Lazy Loading Images

--- Advanced-DOM-and-Events/script.js
+++ Advanced-DOM-and-Events/script.js
@@ -203,7 +203,7 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry); //90
+  //console.log(entry); //90
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden'); //target:section #section--1.section
@@ -219,3 +219,32 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//TITTLE: LAZY LOADING IMAGES
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '+200px', //as we approached the images they are already fully loaded.
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
