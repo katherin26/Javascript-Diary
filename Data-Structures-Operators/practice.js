cb643fff9f464e5757f6167052a3d19a76aed630
katherin <ft.katheochoa@gmail.com>
2021-09-15 16:25:40 -0400

    String Exercises

--- Data-Structures-Operators/practice.js
+++ Data-Structures-Operators/practice.js
@@ -103,3 +103,24 @@ length
 indexOf ===> Correct Answer
 find 
 none*/
+
+/*NOTE: Reduce Object : Consider the following code block. What gets logged?? */
+
+const scrambled = { 2: 'e', 5: 'o', 1: 'h', 4: 'l', 3: 'l' };
+
+const resultS = Object.values(scrambled).reduce((agg, el) => agg + el, '');
+console.log(resultS); // hello
+
+/*Object.values iterates through enumerable integer keys starting from 0 in sequential order. This is the reason
+why the letters appear to sort themselves correctly. 
+
+The reduce method = takes two arguments: a reducer function and a initial value.
+
+In this case, our reducer function is (agg,el) => agg + el and our initial value is "".
+
+In the reducer function, agg is the aggregator (which starts at the initial value) and el is an element in the array
+being iterated over. In this case, as we iterate through the array, we add the current el to the aggregated value.
+
+If our array was ['a','b','c'],then the aggregator would be a after the first iteration, ab the second time, and 
+finally would return abc.
+*/
