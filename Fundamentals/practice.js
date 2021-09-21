0bf3b84fadcc6f83e9ea815fae3a0e7afe6e7329
katherin <ft.katheochoa@gmail.com>
2021-09-21 16:07:27 -0400

    Floating-point numbers

--- Fundamentals/practice.js
+++ Fundamentals/practice.js
@@ -24,3 +24,23 @@ luation of the second expression.
 console.log(typeof Object, typeof Array, typeof Number); // function function function
 
 /*Because Objec, Array, Number are all constructors functions*/
+
+/*NOTE: Consider the following scenario. What gets logged?? */
+
+const A1 = 0.1;
+const B1 = 0.2;
+const C1 = 0.3;
+
+console.log(A1 + B1 === C1); //FALSE
+
+/*
+A1 + B1 = 0.30000000000000004;
+
+Decimal numbers as we know them are most commonly represented using floating-point arithmetic.
+Floating-point numbers only approximate real numbers and cannot accurately represent numbers like 0.1,0.2 or 0.3
+at all. In the case of 0.1 + 0.2, this results in small rounding errors before we event get to the addition part.
+The final sum then equals to 0.30000000000000004.
+
+Similarly, the decimal system cannot represent 1/3 (one third) accurately. And if we took a rounded value like 0.333
+, you wouldn't expect 0.333 + 0.333 + 0.333 to add up to 1,either.
+*/
