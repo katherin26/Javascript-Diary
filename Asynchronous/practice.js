d2152bc883427c37bad4b1a6a30bf4f1f5ca0bdb
katherin <ft.katheochoa@gmail.com>
2021-09-21 16:17:16 -0400

    promise.all

--- Asynchronous/practice.js
+++ Asynchronous/practice.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*NOTE: PROMISE THEN CATCH*/
+
 const p1 = new Promise((resolve, reject) => setTimeout(resolve, 100, 'Hello'));
 
 const p2 = new Promise((resolve, reject) =>
@@ -20,3 +22,23 @@ Promise.race([p1, p2, p3])
 
   In this case, p3 settles first, rejecting after 10 ms and falling into the catch() handler.
   */
+
+/*NOTE: PROMISE.ALL RESOLVE ORDER */
+
+/*We have a timer function that returns a Promise that resolves after a random amount of time. We use Promise.all
+  to resolve an array of timers. What gets logged?? */
+
+const timer = a => {
+  return new Promise(res =>
+    setTimeout(() => {
+      res(a);
+    }, Math.random() * 100)
+  );
+};
+
+const all = Promise.all([timer('first'), timer('second')]).then(data =>
+  console.log(data)
+);
+
+/*The order in which the Promises resolve does not matter to Promise.all. We can reliably count on them to be 
+returned in the same order in which they were provided in the array argument.*/
