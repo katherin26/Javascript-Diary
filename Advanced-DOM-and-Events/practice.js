16352002b37456e211ace8ba08835b26535c97e6
katherin <ft.katheochoa@gmail.com>
2021-09-22 16:13:35 -0400

    Callback setTimeout

--- Advanced-DOM-and-Events/practice.js
+++ Advanced-DOM-and-Events/practice.js
@@ -16,3 +16,38 @@
 /*1 and 4 are displayed first since they are logged by simple calls to console.log() without any delay. 2 is disp-
 lay after 3 because 2 is being logged after a delay of 0 msecs. Note that, despite 3 having a delay of 0 msecs,
 its code will only be executed after the current call stack is cleared. */
+
+/*NOTE: CALLBACK setTimeOut
+Which of the following functions will add two variables and pass the sum to a callback function after 2 seconds?
+*/
+
+//A
+const add = (x, y, callback) => {
+  setTimeout(() => {
+    callback(x + y);
+  }, 2);
+};
+
+//B CORRECT ANSWER
+const add = (x, y, callback) => {
+  setTimeout(() => {
+    callback(x + y);
+  }, 2000);
+};
+
+//C
+const add = (x, y, callback) => {
+  setTimeout(() => {
+    callback(x + y, 2);
+  });
+};
+
+//D
+const add = (x, y, callback) => {
+  setTimeout(() => {
+    callback(x + y, 2000);
+  });
+};
+
+/*The setTimeout function generally takes two variables: the first variable is the function to be executed, and
+the second variable is the interval, in milliseconds, after which the function gets executed. */
