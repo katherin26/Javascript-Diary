a92d4ab4a0743e01f9b81c6ce1ae0b6c9e1e62bb
katherin <ft.katheochoa@gmail.com>
2021-09-23 14:17:24 -0400

    Array methods callbacks

--- Arrays/practice.js
+++ Arrays/practice.js
@@ -417,3 +417,18 @@ arr.push(3);
 console.log(arr);
 
 */
+
+/*NOTE: ARRAY METHOD CALLBACKS : What gets logged in the following code? */
+
+let i = 0;
+
+const arr = new Array(5);
+arr.forEach(() => i++);
+
+console.log(i);
+
+/*Array methods like map, filter, reduce, forEach, etc take a callback as the first parameter, which is executed 
+ for every element in the array. The Array constructor, when given an integer between 0 and (232 - 1), will return 
+ a sparse array, which has a length property equal to the given integer. The returned array is "sparse" because its
+ slots are empty; it does not have any own-properties, other than .length. (In contrast, the array [undefined] does
+ have an own-property of 0, even though the value at that property is undefined.) */
