7f1e6a601ff90cf3c1e45f348a696f99f893b972
katherin <ft.katheochoa@gmail.com>
2021-09-23 22:49:54 -0400

    The event loop in practice

--- Asynchronous-02/script.js
+++ Asynchronous-02/script.js
@@ -6,7 +6,7 @@ setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved Promise 1').then(res => console.log(res));
 
 Promise.resolve('Resolved promise 2').then(res => {
-  for (let i = 0; i < 10000000000; i++) {}
+  for (let i = 0; i < 1000; i++) {}
   console.log(res);
 });
 
