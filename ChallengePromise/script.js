8fb9651f4321b02c761ca321a14f189a37326cb5
katherin <ft.katheochoa@gmail.com>
2021-09-24 23:39:36 -0400

    Challenge 2

--- ChallengePromise/script.js
+++ ChallengePromise/script.js
@@ -101,34 +101,80 @@ const whereAmI2 = function (lat, long) {
 
     .catch(err => console.error(err)); //Handle the erro "Problem with geocoding 403"
 };
-
+/*
 whereAmI2(52.508, 13.381);
 whereAmI2(52.508, 13.381);
 whereAmI2(19.037, 72.873);
-whereAmI2(-33.933, 18.474);
-
-btn.addEventListener('click', whereAmI2);
+whereAmI2(-33.933, 18.474);*/
 
 /* 
 
 CHALLENGE 2
 
-Build the image loading functionality that I just showed you on the screen.
-
-Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
-
 PART 1
-1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
-
-If this part is too tricky for you, just watch the first part of the solution.
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which 
+creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. 
+When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. 
+The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event),
+reject the promise.
 
 PART 2
 2. Comsume the promise using .then and also add an error handler;
 3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
-4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image 
+(HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global 
+  variable for that 😉);
 5. After the second image has loaded, pause execution for 2 seconds again;
 6. After the 2 seconds have passed, hide the current image.
 
-TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to
+'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
 
 */
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+//NOTE: PART 1
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img'); //create a new image.
+    img.src = imgPath; // sets the .src attribute to the provided image path
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error(`Something is wrong!!`));
+    });
+  });
+};
+
+let currentImg;
+//NOTE: PART 2
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2); //This is gonna return a promise.
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
