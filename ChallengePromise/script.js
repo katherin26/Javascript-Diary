5bee67a7955b162075c5a4dd36d00425c1480917
katherin <ft.katheochoa@gmail.com>
2021-09-24 22:35:23 -0400

    img

--- ChallengePromise/script.js
+++ ChallengePromise/script.js
@@ -1,7 +1,11 @@
 'use strict';
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+
 /* 
+
+CHALLENGE 1 
+
 You will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use 
 a second API to geocode coordinates.
 
@@ -104,3 +108,27 @@ whereAmI2(19.037, 72.873);
 whereAmI2(-33.933, 18.474);
 
 btn.addEventListener('click', whereAmI2);
+
+/* 
+
+CHALLENGE 2
+
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+*/
