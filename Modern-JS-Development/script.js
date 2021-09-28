caf809597dcbca40c98a80ebcc36689ee1216d69
katherin <ft.katheochoa@gmail.com>
2021-09-28 15:47:07 -0400

    Module Pattern

--- Modern-JS-Development/script.js
+++ Modern-JS-Development/script.js
@@ -16,3 +16,45 @@ console.log(ShoppingCart.totalPrice);*/
 import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 add('pizza', 2);
 console.log(price);
+
+/*TITTLE: MODULE PATTERN : The main goal of the module pattern is to encapsulate functionality ti have private data
+and to expose a public API and the best way of achieving all that is by simply using a function, because a function
+give us private data by default and allow us to return values, which can become our public API*/
+
+/*IIFE =  The reason is because this way we don't have to call it separately and we can also ensure that it's only
+called once.*/
+
+//This works for closures, remember allow a function to have access to all the variables that were present at its
+//birthplace, so the addToCart function was created in the ShoppingCart2, so therefore this function never loses
+//connection to its birthplace when we return the function.
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart(sipping cost is ${shippingCost})`
+      //shippingCost is accesible !
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('Pizza', 2);
+console.log(ShoppingCart2); //cart: Array(2), totalPrice: 237, totalQuantity: 23, addToCart: ƒ}
+console.log(ShoppingCart2.shippingCost); //Undefined
