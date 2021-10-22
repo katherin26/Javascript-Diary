af0c053246d47a97d595d7bc30dc5367622ee147
katherin <ft.katheochoa@gmail.com>
2021-10-21 23:41:38 -0400

    Find the maximun number in an array

--- Arrays/practice2.js
+++ Arrays/practice2.js
@@ -19,3 +19,21 @@ let arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
 const result = arr.filter(el => el >= 0);
 
 console.log(result);
+
+/*NOTE: Find the maximum number in an array of numbers  */
+
+let arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+let maximum = findMax(arr);
+console.log(`Max: ${maximum}`);
