bc40bd07e0ec80140fa45bd458b9fecd090df438
katherin <ft.katheochoa@gmail.com>
2021-10-21 23:27:30 -0400

    positiveNUm with filter

--- Arrays/practice2.js
+++ Arrays/practice2.js
@@ -11,3 +11,11 @@ function positiveNum(arr) {
 }
 
 console.log(positiveNum([-5, 10, -3, 12, -9, 5, 90, 0, 1]));
+
+/* Filter solution*/
+
+let arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+
+const result = arr.filter(el => el >= 0);
+
+console.log(result);
