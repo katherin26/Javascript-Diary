ee41f5f49714d8c3b70e5c4cb831f7bee763cf86
katherin <ft.katheochoa@gmail.com>
2021-10-22 22:28:51 -0400

    Exercise: create an object and iterate over the object

---
+++ 07-Functions/practice2.js
@@ -0,0 +1,14 @@
+/*NOTE: Create an object which contains street, city and zipcode and then create 
+a function in which you can iterate over the object and print the key and value. */
+
+let area = {
+  street: 'a',
+  city: 'b',
+  zipCode: 'c',
+};
+
+function showAddress(address) {
+  for (let key in address) console.log(key, address[key]);
+}
+
+showAddress(area);
