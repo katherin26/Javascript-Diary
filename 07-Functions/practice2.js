fd2a51f012a9526fa1d2795bf7862e618460fa80
katherin <ft.katheochoa@gmail.com>
2021-10-22 22:53:26 -0400

    Compare objects

--- 07-Functions/practice2.js
+++ 07-Functions/practice2.js
@@ -36,3 +36,36 @@ function createZone(street, city, zipCode) {
 
 let factoryAddress = createZone(`2020 sw 8 ST`, `Miami`, `33225`);
 console.log(factoryAddress);
+
+/*NOTE: Create a function and compare the objects literals, log to the console if they are equals or not. */
+
+let object1 = {
+  name: 'Ninja',
+  lastname: 'Red',
+  age: 50,
+  city: 'D.C',
+};
+
+let object2 = {
+  name: 'Nerd',
+  lastname: 'pink',
+  age: 23,
+  city: 'C.D',
+};
+
+function checkObj(obj1, obj2) {
+  if (obj1 === obj2) {
+    console.log(`They are equals`);
+  } else {
+    console.log(`They are not equals`);
+  }
+  return;
+}
+
+checkObj(object1, object2);
+
+function checkObj2(obj1, obj2) {
+  return obj1 === obj2 ? 'Are equals' : 'Not are equals';
+}
+
+checkObj2(object1, object2);
