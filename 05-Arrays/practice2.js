703475e1f72d9a9fe84de1074fe67af660225c24
katherin <ft.katheochoa@gmail.com>
2021-10-26 19:49:28 -0400

    Insert the dashes

--- 05-Arrays/practice2.js
+++ 05-Arrays/practice2.js
@@ -117,3 +117,19 @@ let firstE = function (arr, n) {
 };
 
 console.log(firstE([7, 7])); //7
+
+/*NOTE: Insert the dashes (-) between the numbers.*/
+
+const numbers = 222886644226;
+const str = numbers.toString();
+const result = [str[0]];
+
+for (let i = 1; i < str.length; i++) {
+  if (str[i - 1] % 2 === 0 && str[i] % 2 === 0) {
+    result.push('-', str[i]);
+  } else {
+    result.push(str[i]);
+  }
+}
+
+console.log(result.join(''));
