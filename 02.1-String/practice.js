10c4c1e1af03bfb845eb35cc4310218bf5485655
katherin <ft.katheochoa@gmail.com>
2021-10-26 20:45:23 -0400

    String basics

---
+++ 02.1-String/practice.js
@@ -0,0 +1,32 @@
+/*NOTE: STRING PRIMITIVE = has no methods.*/
+
+const message = 'This is my first message';
+
+console.log(typeof message); //string
+console.log(message.length); //24 in the background JS interpret the text as an object and
+//that's why you can use methods like length.
+console.log(message[0]); //T Access to the index.
+console.log(message[1]); //h
+console.log(message.includes('my')); //true
+console.log(message.startsWith('This')); //true = Because is the same syntax with uppercase in other case false.
+console.log(message.endsWith('e')); //true
+console.log(message.indexOf('my')); //8 = Is the position in the index with that element.
+console.log(
+  message.replace('first', 'second')
+); /*replace returns a new string and the original
+ remains intact (This is my second message).*/
+console.log(message.toUpperCase()); //Return a new string and the original remains intact.
+console.log(message.trim()); //This removes the white spaces. also you can use (trim left and trim right).
+
+/*NOTE: ESCAPE NOTATION (\') = This became part of the single cuote.*/
+const myString = "This is 'another example";
+console.log(myString); //This is 'another example
+
+/*NOTE: BREAKS (\n) */
+const mySecondString = 'This is \n my second example';
+console.log(mySecondString);
+
+//NOTE: SPLIT()
+
+const myThirdString = 'This is my third example';
+console.log(myThirdString.split(' ')); //[ 'This', 'is', 'my', 'third', 'Example' ]
