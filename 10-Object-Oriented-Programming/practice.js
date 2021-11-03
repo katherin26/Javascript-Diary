ef6c2f0120ff493a5750953fba86dc16dd4b77b1
katherin <ft.katheochoa@gmail.com>
2021-11-03 19:41:30 -0400

    Using constructor Function

--- 10-Object-Oriented-Programming/practice.js
+++ 10-Object-Oriented-Programming/practice.js
@@ -25,3 +25,34 @@ let robot = {
 };
 
 console.log(robot.name); //This prints robot's name = Lucia.
+
+/*Method 2 : Using Constructor Function.
+This function is called constructor function because it constructs objects in this case it 
+creates an object with two properties name and age, such object represents a pet in order 
+for this object to return an object you need to call it with new.
+'this' is a keyword that serves as a placeholder for the object being constructed and it's 
+the one returned at the end.
+Under the hood this is equal to {}.
+*/
+
+/*Behind the scenes this = {} is like pet = {} in method 1.
+It will return the new object created even though there is no return here , If you add a 
+return value, it will ignore whatever you do 'this'.
+
+Remember to call it with new, If you call it without new, it will act as a regular function
+and it will return undefined because the Pet function does not have a return value.
+
+*/
+
+function Pet(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+//Wrong way
+let incorrectWay = Pet('Dug', 8); //Undefined
+
+//Right way, When you add new, it make a constructor call instead of a regular invokation call.
+
+let myFirstPet = new Pet('Lala', 4);
+console.log(myFirstPet); //Pet {name: 'Lala', age:4}
