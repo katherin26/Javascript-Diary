1cf84d3a282ae01182354d0b69a59f0e56784cb9
katherin <ft.katheochoa@gmail.com>
2021-11-06 00:03:32 -0400

    Let and var exercise

---
+++ 01-Fundamentals/practice1.js
@@ -0,0 +1,21 @@
+//NOTE: LET AND VAR EXERCISE
+
+/*Cannot access 'variable0 before initialization'.
+
+console.log(variable0)
+let variable0 = 'a';
+
+*/
+
+/*variable1 is not defined because is out of the scope.
+
+{let variable1 = 'b'};
+console.log(variable1)
+
+*/
+
+//The result is = c.
+{
+  let variable2 = 'c';
+  console.log(variable2);
+}
