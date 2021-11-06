1515deb007b516ffa03ac23bfc654aa570c220a7
katherin <ft.katheochoa@gmail.com>
2021-11-06 00:16:52 -0400

    Scope inside a function

--- 01-Fundamentals/practice1.js
+++ 01-Fundamentals/practice1.js
@@ -27,3 +27,21 @@ therefore when the console.log is printed the result would be undefined.
 
 console.log(variable2);
 var variable2 = 'd'; //undefined
+
+/*SCOPE INSIDE A FUNCTION: 
+What would happen if you replace let with var and use the console.log that 
+it on line 44, the changes would come out normally it would mean that var is 
+accessible from outside. 
+
+NOW: console.log(i) if we print this after the {} for loop , an error would 
+appear since i is only declared in the previous block.
+*/
+
+function start() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  //console.log(i)
+}
+
+start(); //0 1 2 3 4
