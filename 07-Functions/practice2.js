afb3e3374882bfac128798ce9dd76f083046eca1
katherin <ft.katheochoa@gmail.com>
2021-11-08 22:47:57 -0500

    :

--- 07-Functions/practice2.js
+++ 07-Functions/practice2.js
@@ -217,4 +217,20 @@ function sum(...arg) {
 
 console.log(sum([1, 2, 3, 4, 5])); //11
 
-/*NOTE */
+/*NOTE: EXERCISE ERROR HANDLING WITH TRY AND CATCH*/
+
+try {
+  const numbers = [1, 2, 3, 4];
+  const count = countOcurrences(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+function countOcurrences(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error('Invalid array.');
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    return accumulator + occurrence;
+  }, 0);
+}
