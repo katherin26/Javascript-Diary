225f6f6b4aa4456e43342ace7ece2626dc4b902a
katherin <ft.katheochoa@gmail.com>
2021-11-12 14:32:14 -0500

    recursion

--- 07-Functions/practice3.js
+++ 07-Functions/practice3.js
@@ -22,3 +22,19 @@ function countDown(num) {
 }
 
 countDown(5);
+
+/*In the above program, the user passes a number as an argument when calling 
+a function.
+In each iteration, the number value is decreased by 1 and function countDown()
+is called until the numbers is positive. Here, newNumber > 0 is the base condition.
+This recursive call can be explained in the following steps: 
+
+countDown(5) prints 5 and calls countDown(4)
+countDown(4) prints 5 and calls countDown(3)
+countDown(3) prints 5 and calls countDown(2)
+countDown(2) prints 5 and calls countDown(1)
+countDown(1) prints 5 and calls countDown(0)
+
+When the number reaches 0, the base condition is met and the function is not 
+called anymore.
+*/
