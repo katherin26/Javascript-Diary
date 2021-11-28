e6890675a89fbc46003751c9c490f6405e42c141
katherin <ft.katheochoa@gmail.com>
2021-11-28 17:38:22 -0500

    Calculate the average rate

--- 07-Functions/practice4.js
+++ 07-Functions/practice4.js
@@ -254,3 +254,15 @@ function orderAlphabetically(arr) {
 }
 
 console.log(orderAlphabetically(movies));
+
+//Calculate the average rate.
+
+function sumRate(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i].rate;
+  }
+  return sum;
+}
+
+console.log(sumRate(movies)); //45.4
