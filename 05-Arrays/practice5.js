08bc55f471dcf37c70b9c928518565ba416608e1
katherin <ft.katheochoa@gmail.com>
2021-11-30 16:27:13 -0500

    Iterate over an array of names using For Loop

---
+++ 05-Arrays/practice5.js
@@ -0,0 +1,9 @@
+//NOTE: ITERATE OVER AN ARRAY OF NAMES.
+
+let names = ['Pepe', 'Giro', 'Lulu'];
+
+//Method 1 = Regular For Loop.
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+} //Pepe Giro Lulu
