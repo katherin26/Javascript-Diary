4f5e84453c945a3da52d0921a50f4f21876c4f79
katherin <ft.katheochoa@gmail.com>
2021-11-30 16:28:10 -0500

    Iterate over an array of names using For Of

--- 05-Arrays/practice5.js
+++ 05-Arrays/practice5.js
@@ -7,3 +7,11 @@ let names = ['Pepe', 'Giro', 'Lulu'];
 for (let i = 0; i < names.length; i++) {
   console.log(names[i]);
 } //Pepe Giro Lulu
+
+/*Method 2 : Using For Of Loop.
+For of automatically iterates over the values of an array, so you don't need to deal with the key(index).
+*/
+
+for (let name of names) {
+  console.log(name);
+} //Pepe Giro Lulu
