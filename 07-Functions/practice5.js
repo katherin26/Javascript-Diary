5efd80836c6bbb4fbb6a95450aa8e652e9bf2d35
katherin <ft.katheochoa@gmail.com>
2021-12-06 14:11:47 -0500

    Square every digit of a number using map and math pow

--- 07-Functions/practice5.js
+++ 07-Functions/practice5.js
@@ -185,3 +185,26 @@ let resultDivision4 = divide(10, 4, number => {
 });
 
 console.log(resultDivision4); //2.5
+
+/*NOTE: Square every digit of a number, For example, if we run 9119 through the function, 811181 will come out because 92 is 81 and 12 is 1.
+note : The function accepts an integer and returns an integer.
+ */
+
+function squareDigits(num) {
+  return parseInt(
+    num
+      .toString()
+      .split('')
+      .map(n => n * n)
+      .join('')
+  );
+}
+
+console.log(squareDigits(9119));
+
+//Using Math.pow
+function squareDigits2(b, e) {
+  return Math.pow(b, e);
+}
+
+console.log(squareDigits2(9, 2));
