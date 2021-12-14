78dd8063c40158cc963d8cb018de6c332b839902
katherin <ft.katheochoa@gmail.com>
2021-12-14 14:40:46 -0500

    calculating the polygon with class constructor and using get

--- 10-Object-Oriented-Programming/practice2.js
+++ 10-Object-Oriented-Programming/practice2.js
@@ -202,3 +202,23 @@ ejercicio.eatBrunch();
 ejercicio.eatBrunch();
 
 console.log(ejercicio.pesarme()); //Mi peso actual es 72
+
+//NOTE: EXERCISE CLASS CONSTRUCTOR = CALCULATING THE POLYGON
+
+class Poligono {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const cuadrado = new Poligono(10, 10);
+console.log(cuadrado.area); //100
