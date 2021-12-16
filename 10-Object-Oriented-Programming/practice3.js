5465deecd2d9ab61b0b56ef5d89f7a92d7c4c833
katherin <ft.katheochoa@gmail.com>
2021-12-16 14:22:23 -0500

    Using object literal

--- 10-Object-Oriented-Programming/practice3.js
+++ 10-Object-Oriented-Programming/practice3.js
@@ -53,3 +53,40 @@ if (personAge2 >= 18) {
 }
 
 //OUTPUT = Chick can have a beer.
+
+/*NOTE: USING OBJECT LITERAL */
+
+/*An object has the advantage of grouping data that belongs to a single entity and you can also add functionality (methods) that work based on the data the object. */
+
+let person1 = {
+  name: 'Bambi',
+  age: 29,
+  canDrink() {
+    if (this.age >= 18) {
+      return `${this.name} can have a beer.`;
+    } else {
+      return `${this.name} cannot have a beer!`;
+    }
+  },
+};
+
+console.log(person1.canDrink());
+//OUTPUT = Bambi can have a beer.
+
+let person2 = {
+  name: 'Lola',
+  age: 32,
+  canDrink() {
+    if (this.age >= 18) {
+      return `${this.name} can have a beer!`;
+    } else {
+      return `${this.name} cannot have a beer!`;
+    }
+  },
+};
+
+console.log(person2.canDrink());
+
+//OUTPUT = Lola can have a beer!
+
+/*As you can see the code is repetitive and there is no reusability of functionality, you need to manually construct the object every time. */
