be859277fffb0a54fe05162867ac6aac458a0811
katherin <ft.katheochoa@gmail.com>
2021-12-21 18:28:51 -0500

    Continue and break , only string continue

--- 01-Fundamentals/practice1.js
+++ 01-Fundamentals/practice1.js
@@ -222,3 +222,25 @@ for (let i = 0; i < years.length; i++) {
 }
 
 console.log(ages); //[30,14,52,1]
+
+//NOTE: CONTINUE AND BREAK.
+
+//ONLY STRING CONTINUE:
+const jonasArray = [
+  'Jonas',
+  'Schmedmand',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+];
+
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] !== 'string') continue;
+  console.log(jonasArray[i], typeof jonasArray[i]);
+
+  /*
+  Jonas string
+  Schmedmand string
+  teacher string
+   */
+}
