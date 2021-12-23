b455e285d391f3771277e13639226352a0faca01
katherin <ft.katheochoa@gmail.com>
2021-12-23 14:47:33 -0500

    Calculating the temperature

--- 01-Fundamentals/practice1.js
+++ 01-Fundamentals/practice1.js
@@ -414,3 +414,32 @@ const amplitudeNew2 = calcTempAmplitudeNew(temperatures);
 
 console.log(amplitudeNew);
 console.log(amplitudeNew2);
+
+//NOTE:
+
+/*Given an array of forecasted maximun temperatures, the thermometer displays a string with these temperatures.
+
+  Example : [17,21,23] will print "...17C in 1 days..21C in 2 days ... 23C in 3 days..."
+
+  Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+  Use the problem-solving framework : Understand the problem and break it up into sub-problems!!
+
+  TEST DATA 1: [17,21,23]
+  TEST DATA 2: [12,5,-5,0,4]
+*/
+
+const printForecast = function (arr) {
+  let str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}C in ${i + 1} days...`;
+  }
+  console.log(str);
+};
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+printForecast(data1);
+printForecast(data2);
