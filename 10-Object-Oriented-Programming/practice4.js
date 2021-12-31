693ec6cf1401dc9af60b26ce92255168fe388299
katherin <ft.katheochoa@gmail.com>
2021-12-31 16:43:42 -0500

    Enhanced object literals and computed properties

--- 10-Object-Oriented-Programming/practice4.js
+++ 10-Object-Oriented-Programming/practice4.js
@@ -62,3 +62,66 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu) {
   console.log(item);
 }
+
+//NOTE: ENHANCED OBJECT LITERALS AND COMPUTED PROPERTIES.
+
+//ES5
+
+/*We are creating an object. Prior to ES6, if we want to initialize a property in an object using object literals, we will do the following. */
+
+function getLaptop(make, model, year) {
+  return {
+    make: make,
+    model: model,
+    year: year,
+  };
+}
+/*{ make: 'Apple', model: 'Mackbook', year: '2015' } */
+
+let result1 = getLaptop('Apple', 'Mackbook', '2015');
+console.log(result1);
+
+//ES6
+
+/*ES6 Removes all of that repetetition. Here's how we will write our getLaptop function. */
+
+function getLaptop2(make, model, year) {
+  return {
+    make,
+    model,
+    year,
+  };
+}
+
+let result2 = getLaptop2('Acer', 'Laptop', '2021');
+console.log(result2);
+
+/**{ make: 'Acer', model: 'Laptop', year: '2021' } */
+
+//COMPUTED PROPERTIES AND OBJECT LITERALS.
+
+/*There are two ways to specify a key when accessing an object property: the dot notation and bracket notation. The bracket notation allows us to access a property using expressions. Computed property names allows to us write an expressions wrapped in square brackets instead of the regular property name. Whatever the expression evaluates to will become the property name. This means that we can do this.
+
+The value of name was Computed to make and this was used as the name of the property. This is why we can access the property using laptop.make;
+ */
+
+var name = 'make';
+const laptop = {
+  [name]: 'Apple',
+};
+console.log(laptop.make); //Apple
+
+//Similarly, This will work.
+
+var name = 'make';
+var i = 0;
+
+const laptop2 = {
+  [name + ++i]: 'Apple',
+  [name + ++i]: 'Dell',
+  [name + ++i]: 'HP',
+};
+
+console.log(laptop2.make1);
+console.log(laptop2.make2);
+console.log(laptop2.make3);
