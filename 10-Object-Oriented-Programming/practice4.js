240df8822d1e73eb89215fc1b218c361d2d13aec
katherin <ft.katheochoa@gmail.com>
2021-12-31 01:22:54 -0500

    added new practice4 destructuring objects

---
+++ 10-Object-Oriented-Programming/practice4.js
@@ -0,0 +1,24 @@
+//NOTE: Destructuring objects.
+
+const game = {
+  team1: 'chest 1',
+  team2: 'chest 2',
+  players: [
+    ['Gorsssi', 'Pavard', 'Martinez'],
+    ['Brandt', 'Sancho', 'Gotze'],
+  ],
+  score: '4:0',
+  scored: ['Gorsssi', 'Pavard', 'Martinez'],
+  date: 'Nov 9th, 2021',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//Destructuring Object game in player1 and player2 variable.
+
+let [players1, players2] = game.players;
+console.log(players1); //[ 'Gorsssi', 'Pavard', 'Martinez' ]
+console.log(players2); //[ 'Brandt', 'Sancho', 'Gotze' ]
