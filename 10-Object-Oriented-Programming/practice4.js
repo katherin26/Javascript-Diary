491a9dfa325da1a2f344b99bb7e9195c7e6b6970
katherin <ft.katheochoa@gmail.com>
2021-12-31 16:52:13 -0500

    Loop over the game and print each player with the score

--- 10-Object-Oriented-Programming/practice4.js
+++ 10-Object-Oriented-Programming/practice4.js
@@ -125,3 +125,52 @@ const laptop2 = {
 console.log(laptop2.make1);
 console.log(laptop2.make2);
 console.log(laptop2.make3);
+
+//NOTE: LOOP OVER AN OBJECT WITH ARRAYS USING FOR OF AND ENTRIES.
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//NOTE: Loop over the game.scored array and print each player name to the console, along with the goal number (example: 'Goal 1 : Lewandowski');
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1} : ${player}`);
+}
