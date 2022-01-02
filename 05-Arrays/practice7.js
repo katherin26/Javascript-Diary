0cd699c1c8c43808f53f0ae69aa8f056488fde87
katherin <ft.katheochoa@gmail.com>
2022-01-02 18:50:47 -0500

    exercise with set

---
+++ 05-Arrays/practice7.js
@@ -0,0 +1,61 @@
+/*
+1.Create an array 'events' of the different game events that happened (no duplicates).
+2.After the game has finished , is was found that the yellow card from minute 64 was unfair . So remove this event from the game event log.
+3.Print the following string to the console; : "An event happened , on average , every 9 minutes" (keep in mind that a game has 90 minutes)
+4.Loop over the events and log them to the console , marking whether it's in the first half or second half (after 45 min) of the game , like this:
+     [FIRST HALF] 17 : GOAL*/
+
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow Card'],
+  [69, 'Red Card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow Card'],
+]);
+
+//SOLUTION
+//1.
+
+const event = [...new Set(gameEvents.values())];
+
+console.log(event);
+
+//[ 'GOAL', 'Substitution', 'Yellow Card', 'Red Card' ]
+
+//2.
+
+gameEvents.delete(64); //true
+
+//3.
+
+const time = [...gameEvents.keys()].pop();
+console.log(time); //92
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+//An event happened, on average, every 9.2 minutes
+
+//4.
+
+for (const [min, value] of gameEvents) {
+  const half = min <= 45 ? 'First' : 'Second';
+  console.log(`${half} [half] ${min} : ${event}.`);
+}
+
+/*First [half] 17 : GOAL,Substitution,Yellow Card,Red Card.
+      First [half] 36 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 47 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 61 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 69 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 70 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 72 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 76 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 80 : GOAL,Substitution,Yellow Card,Red Card.
+      Second [half] 92 : GOAL,Substitution,Yellow Card,Red Card. */
