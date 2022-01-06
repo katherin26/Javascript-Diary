23a381897655f9e55b3f4048ca0665a362d1e0fc
katherin <ft.katheochoa@gmail.com>
2022-01-06 15:17:26 -0500

    Copy by value using primitive values

---
+++ 01-Fundamentals/practice2.js
@@ -0,0 +1,31 @@
+//NOTE: COPY BY VALUE USING PRIMITIVE VALUES LIKE
+
+//NUMBER.
+
+let age = 20;
+let ageCopy = age;
+
+ageCopy = 35;
+
+console.log(age); //20
+console.log(ageCopy); //35
+
+//BOOLEAN
+
+let boolean = true;
+let booleanCopy = boolean;
+
+booleanCopy = false;
+
+console.log(boolean); //true
+console.log(booleanCopy); //false
+
+//STRING
+
+let string = 'Original Name';
+let stringCopy = string;
+
+stringCopy = 'Copy Name';
+
+console.log(string); //Original Name
+console.log(stringCopy); //Copy Name
