31fc25c3e218b88d23c05ec646d676f6947053f0
katherin <ft.katheochoa@gmail.com>
2022-01-21 13:59:47 -0500

    The while Loop

--- 25-Exercices-EloquentJs/script.js
+++ 25-Exercices-EloquentJs/script.js
@@ -7,3 +7,15 @@ while (number <= 12) {
   number = number + 2;
 }
 /*0 2 4 6 8 10 12 */
+
+//The while Loop.
+
+let result = 1;
+let counter = 0;
+
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+
+console.log(result); //1024
