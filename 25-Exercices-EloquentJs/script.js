9c3cf2b9d8dc33df4a56fd3622465838c4f49e11
katherin <ft.katheochoa@gmail.com>
2022-01-21 14:03:39 -0500

    FizzBuzz

--- 25-Exercices-EloquentJs/script.js
+++ 25-Exercices-EloquentJs/script.js
@@ -40,3 +40,19 @@ for (let i = '#'; i.length < 12; i += '#') {
    ###########
    
     */
+
+/*NOTE:  Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+
+For numbers divisible by 3, print "Fizz" instead of the number, and 
+
+For numbers divisible by 5 and not 3 print "buzz" instead . 
+
+When you have that working modify your program to print "fizzBuzz" for numbers divisible by only one of those.*/
+
+for (let i = 0; i <= 100; i++) {
+  let output = '';
+
+  if (i % 3 == 0) output += 'Fizz';
+  if (i % 5 == 0) output += 'Buzz';
+  console.log(`${i} ${output}`);
+}
