627e1f3cabca925fde075ad5bae39330cdcda6e8
katherin <ft.katheochoa@gmail.com>
2022-01-23 21:40:20 -0500

    Even or Odd using recursion

--- 07-Functions/practice8.js
+++ 07-Functions/practice8.js
@@ -5,3 +5,21 @@ function min(num1, num2) {
 }
 
 console.log(min(2, 3));
+
+//NOTE: Even or odd using recursion.
+
+function isEven(num) {
+  if (num == 0) {
+    return true;
+  } else if (num == 1) {
+    return false;
+  } else if (num < 0) {
+    return isEven(-num);
+  } else {
+    return isEven(num - 2);
+  }
+}
+
+console.log(isEven(50)); //true
+console.log(isEven(75)); //false
+console.log(isEven(-1)); //false
