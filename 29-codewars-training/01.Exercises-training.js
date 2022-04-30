359e87058592fe6717f984d103ab098c23465a4e
Katherin26 <ft.katheochoa@gmail.com>
2022-04-30 16:34:17 -0400

    added exercices training in codewars

---
+++ 29-codewars-training/01.Exercises-training.js
@@ -0,0 +1,19 @@
+/*Complete the method that takes a boolean value and return a "Yes" string for true, 
+or a "No" string for false.*/
+
+function boolToWord(bool) {
+  return bool ? true ? "yes" : "No";
+}
+
+
+/*Write a function that takes an array of words and smashes them together into a sentence
+ and returns the sentence. You can ignore any need to sanitize words or add punctuation, 
+ but you should add spaces between each word. Be careful, there shouldn't be a space at the
+  beginning or the end of the sentence!*/
+
+
+  const words = ["hello","world","this","is","great"]
+
+  function arr(arr){
+      return  arr.join(' ');
+  }
