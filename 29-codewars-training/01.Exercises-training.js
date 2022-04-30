6cc4c5520c9fa4b926060c429dbea9d0953a67b6
Katherin26 <ft.katheochoa@gmail.com>
2022-04-30 16:46:34 -0400

    added exercises kata training check the number

--- 29-codewars-training/01.Exercises-training.js
+++ 29-codewars-training/01.Exercises-training.js
@@ -35,3 +35,20 @@ function find_average(arr){
     }
     return sum/ arr.length;
   }
+
+/*You will be given an array a and a value x. All you need to do is check whether the 
+provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.*/
+
+let arr = [66,101];
+let arr2 = [101,45,75,105,99,107];
+
+function check(a,x){
+    for(let i = 0 ; i < a.length; i++){
+        if(a[i] === x) return "true";
+        else "false";
+    }
+}
