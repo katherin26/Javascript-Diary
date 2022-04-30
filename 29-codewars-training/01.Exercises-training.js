b73d991770fd1de3dda5713e08f3c7e398c59a31
Katherin26 <ft.katheochoa@gmail.com>
2022-04-30 16:36:34 -0400

    added double interger

--- 29-codewars-training/01.Exercises-training.js
+++ 29-codewars-training/01.Exercises-training.js
@@ -17,3 +17,9 @@ function boolToWord(bool) {
   function arr(arr){
       return  arr.join(' ');
   }
+
+  /*Code as fast as you can! You need to double the integer and return it.*/
+
+  function doubleInteger(i){
+      return i * 2;
+  }
