abf07ad1f8c67876b0739fd120f34d6e839f7cfe
Katherin26 <ft.katheochoa@gmail.com>
2022-04-30 16:59:44 -0400

    added findWord

--- 29-codewars-training/01.Exercises-training.js
+++ 29-codewars-training/01.Exercises-training.js
@@ -72,3 +72,31 @@ function sum(numbers){
     return sum;
 
 }
+
+/*Simple, remove the spaces from the string, the return the resultant string.*/
+
+function noSpace(x){
+    let arrX = x.split(" ").join('');
+    return arrX;
+}
+
+console.log(noSpace('a s d f e g t dd s s'));
+
+/*Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so: */
+
+function findWord(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === "needle"){
+            console.log(`Found the needle at position.`, i)
+        }
+    }
+}
+
+console.log(findNeedle(['moreJunk','needle']))
+
