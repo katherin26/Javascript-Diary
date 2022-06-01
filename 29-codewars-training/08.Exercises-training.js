40ff0199c61cfb56435485b5604b8f37533cae03
Katherin26 <ft.katheochoa@gmail.com>
2022-06-01 14:37:19 -0400

    added Kata

--- 29-codewars-training/08.Exercises-training.js
+++ 29-codewars-training/08.Exercises-training.js
@@ -99,3 +99,24 @@ function forLoopBack(num) {
 }
 
 console.log(forLoopBack(10));
+
+/*Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.*/
+
+function invert(array) {
+  let arr = [];
+  for (let value of array) {
+    if (value >= 0) {
+      arr.push(parseInt('-' + value));
+    } else {
+      arr.push(Math.abs(value));
+    }
+  }
+  return arr;
+}
+
+console.log(invert([-1, 1, 2, 3, 4, 5]));
