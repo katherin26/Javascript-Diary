712482a13f8fb57b8e94a0a33407fdb02388a2b7
Katherin26 <ft.katheochoa@gmail.com>
2022-06-02 16:35:54 -0400

    added katas

--- 29-codewars-training/08.Exercises-training.js
+++ 29-codewars-training/08.Exercises-training.js
@@ -120,3 +120,106 @@ function invert(array) {
 }
 
 console.log(invert([-1, 1, 2, 3, 4, 5]));
+
+/**** No Loops Allowed ***
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+*/
+
+function check(a, x) {
+  return a.includes(x);
+}
+
+console.log(check([66, 101], 66));
+
+/*Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+Examples (Input -> Output)
+15  -> '101.25 Chinese Yuan'
+465 -> '3138.75 Chinese Yuan'
+The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")*/
+
+function usdcny(usd) {
+  let result = usd * 6.75;
+  return result.toFixed(2);
+}
+
+console.log(usdcny(2));
+
+/*Complete the solution so that it reverses the string passed into it*/
+
+function solution(str) {
+  let arr = str.split('');
+  return arr.reverse().join('');
+}
+
+console.log(solution('world'));
+
+/*In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.nu*/
+
+function makeNegative(num) {
+  if (num === 0) {
+    return num;
+  } else if (num > 0) {
+    return parseInt(`-${num}`);
+  } else {
+    return num;
+  }
+}
+
+console.log(makeNegative(15));
+console.log(makeNegative(-15));
+console.log(makeNegative(0));
+
+/*Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+  let x = [];
+  let y = [];
+  let total = 0;
+
+  for (const value of games) {
+    let arr = value.split('');
+    x.push(arr[0]);
+    y.push(arr[2]);
+  }
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] > y[i]) {
+      total += 3;
+    } else if (x[i] < y[i]) {
+    } else {
+      total += 1;
+    }
+  }
+  return total;
+}
+
+console.log(points(['1:0', '2:0', '3:0', '0:0']));
