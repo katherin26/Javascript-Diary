c01920bc39520f367d48678cd12cbe6551dcd41c
Katherin26 <ft.katheochoa@gmail.com>
2022-06-09 18:37:43 -0400

    added description

--- 31-algorithms-dataStructures/ProblemSolving.js
+++ 31-algorithms-dataStructures/ProblemSolving.js
@@ -1,4 +1,7 @@
 /*Frecuency Counter*/
+/*Given two strings, write a function to determine if the second string is an anagram 
+of the first. An anagram is a word, phrase, or name formed by rearranging the letters of 
+another, such as cinema, formed from iceman.*/
 
 function countLetters(str1, str2) {
   if (str1.length !== str2.length) return false;
