56c57912331a9309d7f062152603d1fe235b29b3
Katherin26 <ft.katheochoa@gmail.com>
2022-06-09 18:36:56 -0400

    added frecuency county pattern

---
+++ 31-algorithms-dataStructures/ProblemSolving.js
@@ -0,0 +1,35 @@
+/*Frecuency Counter*/
+
+function countLetters(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let firstStr = {};
+  let secondStr = {};
+
+  let arrString1 = str1.split('');
+  let arrString2 = str2.split('');
+
+  for (let i = 0; i < arrString1.length; i++) {
+    if (firstStr[arrString1[i]]) {
+      firstStr[arrString1[i]]++;
+    } else {
+      firstStr[arrString1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < arrString2.length; i++) {
+    if (secondStr[arrString2[i]]) {
+      secondStr[arrString2[i]]++;
+    } else {
+      secondStr[arrString2[i]] = 1;
+    }
+  }
+
+  for (let firstProperty in firstStr) {
+    if (firstStr[firstProperty] !== secondStr[firstProperty]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(countLetters('ada', 'ada'));
