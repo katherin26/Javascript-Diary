cd2a3aeaee9055b92c4aff1ea445d65492d77b9b
Katherin26 <ft.katheochoa@gmail.com>
2022-06-15 19:35:47 -0400

    added recursion exercises

--- 31-algorithms-dataStructures/03.Recursion.js
+++ 31-algorithms-dataStructures/03.Recursion.js
@@ -183,3 +183,62 @@ console.log(factorial(4)); //24
   
   factorial(0) return 1;
   */
+
+/*Write a function called productOfArray and return the product of the array.*/
+
+function productOfArray(arr) {
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return arr[0];
+  return arr[0] * productOfArray(arr.slice(1));
+}
+console.log(productOfArray([1, 2, 3, 4, 5]));
+
+/*
+arr[0] 1 * productOfArray([2,3,4,5]) ---> 1 * 120 = 120
+arr[0] 2 * productOfArray([3,4,5]) ---> 2 * 60 = 120
+arr[0] 3 * productOfArray([4,5]) ---> 3 * 20 = 60
+arr[0] 4 * productOfArray([5])---> 5 * 4 = 20
+*/
+
+/*Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.*/
+
+function recursiveRange(num) {
+  if (num === 0) return num;
+  return num + recursiveRange(num - 1);
+}
+
+console.log(recursiveRange(4));
+
+/*
+4 + (3) = 4 + 6 = 10
+3 + (2) = 3 + 3 = 6
+2 + (1) = 2 + 1 = 3 
+1 + (0) = 1 + 0 = 1
+*/
+
+/*Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence, Recall that the Fibonacci sequence is the sequence of whole numbers 1,1,2,3,5,8... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.*/
+
+function fubonacci(nth) {
+  if (nth <= 2) return 1;
+
+  return fubonacci(nth - 1) + fubonacci(nth - 2);
+}
+
+console.log(fubonacci(7)); //13
+
+/*
+f(7)
+
+    f(6) + f(5) = 8 + 5 = 13
+f(6) 
+    f(5) + f(4) = 5 + 3 = 8
+f(5)
+    f(4) + f(3) = 3 + 2 = 5
+f(4)
+    f(3) + f(2) = 2 + 1 = 3 
+f(3) 
+    f(2) + f(1) = 1 + 1 = 2 
+
+f(2) Return 1
+f(1) Return 1
+*/
