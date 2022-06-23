001e92952c5dee553255d644d88dc9b0674ba46f
Katherin26 <ft.katheochoa@gmail.com>
2022-06-22 22:44:22 -0400

    added insertion sort algorithm

--- 31-algorithms-dataStructures/07.InsertionSort.js
+++ 31-algorithms-dataStructures/07.InsertionSort.js
@@ -11,3 +11,22 @@ function insertionSort(arr) {
 }
 
 insertionSort([2, 1, 9, 76, 4]);
+
+/*Second exercise */
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4]));
